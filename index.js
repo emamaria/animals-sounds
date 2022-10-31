f2d1b627766e62741cf067ed8fc0cb8d220baff5
emamaria <emascript@gmail.com>
2022-10-31 17:32:53 +0100

    changed styles and pictures

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ console.log(animals.length)
 
 let mainContainer = document.querySelector(".animals_container")
 
+let title = document.querySelector("h1")
+
 
 animals.forEach( elem => {
  let animal =  document.createElement("img");
  animal.setAttribute("src", `images/${elem}.jpg`)
  animal.setAttribute("class", `animal`)
  animal.setAttribute("alt", `${elem}`)
+ animal.style.width = "300px"
+ animal.style.margin = "5px"
+
+ 
+ 
  return mainContainer.insertBefore(animal, null)
 }
 )
 
+mainContainer.style.display = "flex"
+mainContainer.style.flexWrap = "wrap"
+mainContainer.style.justifyContent = "center"
 
  let allAnimals = document.querySelectorAll(".animal")
 
